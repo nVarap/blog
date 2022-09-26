ba0f693c64876b0ad85fca3eab59c95274a53af5
nVarap <nvarap@gmail.com>
2022-09-25 21:34:30 -0700

    why

---
+++ js_for_your_viewing_pleasure/helloworld/main.js
@@ -0,0 +1,9 @@
+console.log("this is *hopefully* a substitute to the notebook hacks");
+console.log("this shows that javascript is indeed working, rather that");
+console.log("it isn't working in vscode/jupyter notebooks");
+
+function func(msg){
+    console.log(msg+", Hello world");
+}
+
+func("Varalu")
